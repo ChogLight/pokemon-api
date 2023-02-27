5926524e4388ce66ac2d50e6f69b1501c42c3a39
ChogLight <sebas8812@gmail.com>
2023-02-26 20:26:24 -0500

    configured cors

--- server.js
+++ server.js
@@ -10,11 +10,14 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('connected to database'))
 
+var corsOptions = {
+    origin: 'http://localhost:5173/pokedex',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 
 app.use(express.json())
-app.use(cors())
 const pokemonTeamRouter = require('./routes/pokemonTeam.js')
 
 
-app.use('/poketeam', pokemonTeamRouter)
+app.use('/poketeam', cors(corsOptions), pokemonTeamRouter)
 app.listen(3000, () => console.log('Connected to server'))
