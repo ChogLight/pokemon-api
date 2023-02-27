a7f3e8426c1895261e3044d2956d963513575794
ChogLight <sebas8812@gmail.com>
2023-02-26 22:27:39 -0500

    configured cors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true})
 const db = mongoose.connection
@@ -13,7 +14,7 @@ db.once('open', () => console.log('connected to database'))
 
 app.use(cors())
 app.use(express.json())
-app.use(express.bodyParser({limit: '50mb'}))
+app.use(bodyParser({limit: '50mb'}))
 const pokemonTeamRouter = require('./routes/pokemonTeam.js')
 
 
