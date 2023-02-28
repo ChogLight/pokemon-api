1979384cfb83fc5d5d38146bf4804d981e0253eb
ChogLight <sebas8812@gmail.com>
2023-02-28 15:42:21 -0500

    changed listen server order

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 app.use(bodyParser.json({limit: '500mb'}));
 app.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
-app.listen(3000, () => console.log('Connected to server'))
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true})
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true}).then(() => app.listen(3000, () => console.log('Connected to server')))
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('connected to database'))
