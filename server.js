3101e9b8e1e454e708351f124d946e6a6e850695
ChogLight <sebas8812@gmail.com>
2023-02-28 15:56:55 -0500

    changed listen server order

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.DB_URL);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
 app.use(bodyParser.json({limit: '500mb'}));
 app.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true}).then(() => app.listen(3000, () => console.log('Connected to server')))
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('connected to database'))
@@ -16,6 +24,10 @@ db.once('open', () => console.log('connected to database'))
 app.use(cors())
 app.use(express.json())
 const pokemonTeamRouter = require('./routes/pokemonTeam.js')
-
-
 app.use('/poketeam', pokemonTeamRouter)
+
+connectDB().then(() => {
+    app.listen(3000, () => {
+        console.log("listening for requests");
+    })
+})
